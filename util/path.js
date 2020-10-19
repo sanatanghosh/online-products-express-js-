6d87f893980300794f35c3639169377c5df0e1a6
sanatanghosh <sanatanghosh1997@gmail.com>
2020-10-19 19:10:21 +0530

    models and controllers added

--- util/path.js
+++ util/path.js
@@ -1,3 +1,7 @@
 const path = require('path');
 
-module.exports = path.dirname(process.mainModule.filename);
+
+module.exports = path.dirname(process.mainModule.filename)
+
+
+// module.exports = path.dirname(process.require.main.filename);
