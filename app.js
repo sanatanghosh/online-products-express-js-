60895030405090590fa0bab0b90558c4b3562d8d
sanatanghosh <sanatanghosh1997@gmail.com>
2020-10-31 14:25:38 +0530

    mongo-db crud operation done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoConnect = require ('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -31,13 +33,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    // User.findById(1)
-    //   .then(user => {
-    //     req.user = user;
-    //     next();
-    //   })
-    //   .catch(err => console.log(err));
-    next();
+    User.findById("5f9bb2aea5198cd5d6fc0f4c")
+      .then(user => {
+        req.user = new User(user.name,user.email,user.cart,user._id);
+        next();
+      })
+      .catch(err => console.log(err));
   });
 
 app.use('/admin', adminRoutes);
@@ -46,6 +47,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(()=> {
+
     app.listen(3000);
 });
 
--- models/product.js
+++ models/product.js
@@ -3,18 +3,28 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl,id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
   save() {
     const db = getDb();
-    return db
+    let dbOp;
+    if (this._id)
+    {
+      dbOp = db
       .collection('products')
-      .insertOne(this)
+      .updateOne({ _id: this._id}, { $set: this });
+    }
+    else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -52,6 +62,21 @@ class Product {
         console.log(err);
       });
   }
+
+  static deleteById (prodId) {
+    const db = getDb();
+    return db
+    .collection('products')
+    .deleteOne({_id: new mongodb.ObjectId(prodId)})
+    .then ( result => {
+      console.log('Deleted');
+    })
+    .catch ( err => {
+      console.log(err)
+    });
+  }
+
+
 }
 
 module.exports = Product;
